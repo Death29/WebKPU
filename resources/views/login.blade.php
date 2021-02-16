6d57a185fdfd8010d2577bb4df49b73c1cfbea64
khukuh anugrah <khukuh.anugrah@gmail.com>
2021-02-16 17:55:45 +0700

    Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -36,7 +36,7 @@
 			<div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
 				<form class="login100-form validate-form flex-sb flex-w">
 					<span class="login100-form-title p-b-53">
-						Login Akun UII
+						Login Dengan Akun UII
 					</span>
 
 					<!-- <a href="#" class="btn-face m-b-20">
