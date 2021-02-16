dc5003447f1a0fe7ab9a70c75f10cf7a48e5382b
khukuh anugrah <khukuh.anugrah@gmail.com>
2021-02-16 17:48:31 +0700

    Login (sudah install laravel socialite tapi blum utk api nya)

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+memanggil halaman view sebagai view di tampilan
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function(){
+    return view('login');
+});
